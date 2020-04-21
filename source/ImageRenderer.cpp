483e7f9d54b225ac2713e14ad0398d9f01c3c62a
zz <xzrunner@gmail.com>
2020-04-21 08:51:22 +0800

    [FIXED] init blend state

--- source/ImageRenderer.cpp
+++ source/ImageRenderer.cpp
@@ -30,6 +30,12 @@ void ImageRenderer::Draw(const ur2::Device& dev, ur2::Context& ctx,
     rs.depth_test.enabled = false;
     rs.facet_culling.enabled = false;
 
+    rs.blending.enabled = true;
+    rs.blending.separately = false;
+    rs.blending.src = ur2::BlendingFactor::One;
+    rs.blending.dst = ur2::BlendingFactor::OneMinusSrcAlpha;
+    rs.blending.equation = ur2::BlendEquation::Add;
+
     sm::Matrix2D mt;
     for (auto& r : m_renderable)
     {
